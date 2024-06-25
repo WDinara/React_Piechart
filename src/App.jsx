e875e798e9375dbac8a1a2a52ff5cec3a0f56768
Dinara <dilinguistfrgf@gmail.com>
2024-06-25 19:15:48 +0200

    Initial comment

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Dance"],
+  ["Walk", 11],
+  ["Squats", 2],
+  ["Jogging", 2],
+  ["Yoga", 2],
+  ["Stairs", 7],
+];
+
+export const options = {
+  title: 'Physical activities',
+};
+
+export function App() {
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  );
+}
